139c4ee30189060aac191b0c6f81e52274439884
s-sawaguchi <sawaguchi0604@gmail.com>
2018-11-06 23:25:51 +0900

    [add]add FireaseRestAPIs

---
+++ Assets/Game/Scripts/Models/FirebaseRestAPI.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UniRx;
+using SimpleFirebaseUnity;
+
+/// <summary>
+/// FirebaseDBへのアクセッサモデル
+/// </summary>
+public class FirebaseRestAPI {
+
+    Firebase _firebase;
+    ReactiveDictionary<string, FirebaseObserver> _observers = new ReactiveDictionary<string, FirebaseObserver>();
+
+    static FirebaseRestAPI instance = new FirebaseRestAPI();
+
+    private FirebaseRestAPI()
+    {
+    }
+
+    public static FirebaseRestAPI Instance
+    {
+        get { return instance;}
+    }
+
+    public void CreateNewConnect(string url)
+    {
+        _firebase = Firebase.CreateNew(url);
+    }
+
+    public void ConnectStart()
+    {
+        foreach (FirebaseObserver observer in _observers.Values) {
+            observer.Start();
+        }
+    }
+    
+
+    /// <summary>
+    /// DBの値が変更されたコールされるコールバック
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="OnChangeValue"></param>
+    public void AddObserbser(string name, Action<Firebase, DataSnapshot> OnChangeValue)
+    {
+
+        Firebase child = _firebase.Child(name);
+        child.OnGetSuccess += OnChangeValue;
+        child.GetValue("print=pretty");
+
+        _observers.Add(name, new FirebaseObserver(child, 1f));
+        _observers[name].OnChange += OnChangeValue;
+        _observers[name].Start();
+    }
+
+    /// <summary>
+    /// 値を取得
+    /// </summary>
+    /// <param name="root"></param>
+    /// <param name="value"></param>
+    public void SetValue(string root, string value)
+    {
+        _firebase.Child(root).SetValue(value, true);
+    }
+
+    /// <summary>
+    /// 値を取得
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetValue(string value)
+    {
+        _firebase.SetValue(value, true);
+    }
+
+}
---
+++ Assets/Game/Scripts/Presenters/GamePresenter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePresenter : MonoBehaviour {
+
+    FirebaseRestAPI _dbReceiver = FirebaseRestAPI.Instance;
+
+    void Start () {
+
+        _dbReceiver.CreateNewConnect("https://virtualsmartroom-307ba.firebaseio.com/");
+
+        _dbReceiver.AddObserbser("light", (sender, data) =>
+        {
+            Debug.Log(data.RawValue);
+        });
+    }
+	
+}
