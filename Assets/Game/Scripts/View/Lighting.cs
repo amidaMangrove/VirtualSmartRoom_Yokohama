af39c53c5d218e8c26e296c8bdb1bc4d559772d6
s-sawaguchi <sawaguchi0604@gmail.com>
2018-11-06 23:49:03 +0900

    [add]add lighting script

---
+++ Assets/Game/Scripts/View/Lighting.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class Lighting : MonoBehaviour {
+
+    [SerializeField] float min = 0.0f;
+    [SerializeField] float max = 1.6f;
+
+    Light _light;
+    float intencity;
+	// Use this for initialization
+	void Start () {
+        _light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _light.intensity = Mathf.Lerp(_light.intensity, intencity, Time.deltaTime);
+	}
+
+    public void UpdateLight(string flg)
+    {
+        intencity = (flg == "on") ? max : min;
+    }
+}
